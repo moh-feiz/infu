3edcd7111eaaa0c4d2d69fd1535c693185c63364
moh-feiz <46044724+moh-feiz@users.noreply.github.com>
2018-12-24 12:51:27 +0330

    change website form for four pics

--- frontend/views/website/_form.php
+++ frontend/views/website/_form.php
@@ -11,6 +11,7 @@ use common\models\Image;
 /* @var $model common\models\Website */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+<link href="<?= Yii::getAlias('@web').'/css/website/form.css'?>" type="text/css" rel="stylesheet">
 
 <div class="website-form">
 
@@ -18,7 +19,7 @@ use common\models\Image;
 
 
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true ]) ?>
 
     <?= $form->field($model, 'type')->dropDownList([  '0' => 'نفت و گاز', '1' => 'نیروگاهی' , '2' =>'راهسازی' , '3' => 'ایرلاین']) ?>
 
@@ -26,7 +27,14 @@ use common\models\Image;
 
     <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'image')->fileInput(['name'=>'image']) ?>
+    <?= $form->field($model, 'image')->fileInput(['name'=>'image[]'])->label('images') ?>
+    <?= $form->field($model, 'image')->fileInput(['name'=>'image[]', 'class'=>'display-img-2'])->label(false) ?>
+    <?= $form->field($model, 'image')->fileInput(['name'=>'image[]', 'class'=>'display-img-3'])->label(false) ?>
+    <?= $form->field($model, 'image')->fileInput(['name'=>'image[]', 'class'=>'display-img-4'])->label(false) ?>
+
+    <a href="#" class="show-next display-btn-2 btn btn-info">Add next image</a>
+    <a href="#" class="display-btn-3 btn btn-primary" id="show-next">Add Next image</a>
+    <a href="#" class="display-btn-4 btn btn-danger">Add Next image</a>
     
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
@@ -34,6 +42,11 @@ use common\models\Image;
 
     <?php ActiveForm::end(); ?>
 
+
+
 </div>
+
+
+<script src="<?= Yii::getAlias('@web').'/js/website/form.js'?>"></script>
 
 
